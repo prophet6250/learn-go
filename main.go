5dd529c47f2c83f1978ce00ca4168418fcd4e1af
Kumar Tatsat <kumar.tatsat@loconav.com>
2023-04-16 01:59:29 +0530

    Mini project: calculator
    - also refactor comments to be on top of code, and not side

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"math"
+	"os"
 )
 
 func main() {
@@ -9,15 +13,21 @@ func main() {
 	// values()
 	// variables()
 	// loops()
-	conditions()
+	// conditions()
+	calculator()
 }
 
 func values() {
-	// fmt.Println("hello" + "world") // string concatenation works out of the box :)
-	fmt.Println("1+1 =", 68+1) // comma separated values are concatenated in string
-	fmt.Println("7/3", 7/3)    // automatic type inference. 7/3 is int, 7.0/3.0 is float or double
-	fmt.Println("7/3", 7/3.0)  // automatic type inference. 7/3 is int, 7/3.0 is float or double
-	fmt.Println(true || false) // boolean operations work just like C-like languages
+	// string concatenation works out of the box :)
+	fmt.Println("hello" + "world")
+	// comma separated values are concatenated in string
+	fmt.Println("1+1 =", 68+1)
+	// automatic type inference. 7/3 is int, 7.0/3.0 is float or double
+	fmt.Println("7/3", 7/3)
+	// automatic type inference. 7/3 is int, 7/3.0 is float or double
+	fmt.Println("7/3", 7/3.0)
+	// boolean operations work just like C-like languages
+	fmt.Println(true || false)
 	fmt.Println(!false)
 }
 
@@ -33,9 +43,11 @@ func variables() {
 	another_string := "another string"
 	fmt.Println(another_string)
 
-	fmt.Println(constantString) // basic ass constant variable printing
+	// basic ass constant variable printing
+	fmt.Println(constantString)
 
-	const chargeOfElectron float64 = 1.602176634 / 1e19 // can't convert scientific notation containing negative exponents to string
+	// can't convert scientific notation containing negative exponents to string
+	const chargeOfElectron float64 = 1.602176634 / 1e19
 	fmt.Println(chargeOfElectron)
 }
 
@@ -54,14 +66,16 @@ func loops() {
 }
 
 func conditions() {
-	if 69 == 69.0 { // implicit type conversion
+	// implicit type conversion
+	if 69 == 69.0 {
 		fmt.Println("joe mama gay")
 	} else {
 		fmt.Println("who says I'm gay?\nyou are gay")
 	}
 
 	i := 10
-	switch i { // this will check for values of i, won't allow conditions
+	// this will check for values of i, won't allow conditions
+	switch i {
 	case 0:
 		fmt.Println("this is 0")
 	case 5:
@@ -69,8 +83,8 @@ func conditions() {
 	case 10:
 		fmt.Println("this is 10")
 	}
-
-	switch { // switch without variable will allow conditions over any variable as well
+	// switch without variable will allow conditions over any variable as well
+	switch {
 	case i < 5:
 		fmt.Println("this is < 5")
 	case i < 10:
@@ -78,4 +92,91 @@ func conditions() {
 	case i < 100:
 		fmt.Println("this is < 100")
 	}
+}
+
+func calculator() {
+	// just like java, get a reader, then read provide a source
+	reader := bufio.NewReader(os.Stdin)
+	// I don't want any buffer overflow attacks, F U
+	byteOperator, _ := reader.ReadByte()
+	// will convert the ASCII representation of char into string
+	stringOperator := string(byteOperator)
+
+	var operand1, operand2, result float32
+	var err error
+
+	// C-like input scanners available, check io and bufio as well
+	fmt.Scanf("%f%f", &operand1, &operand2)
+
+	switch stringOperator {
+	case "+":
+		result, err = add(operand1, operand2)
+	case "-":
+		result, err = subtract(operand1, operand2)
+	case "*":
+		result, err = multiply(operand1, operand2)
+	case "/":
+		result, err = divide(operand1, operand2)
+	default:
+		fmt.Println("Invalid Operand")
+	}
+
+	if err != nil {
+		fmt.Println("ERROR:", err.Error())
+	} else {
+		// string formatters work just like C, nice
+		fmt.Printf("%f %s %f = %f\n", operand1, stringOperator, operand2, result)
+	}
+}
+
+func add(a float32, b float32) (float32, error) {
+	valid, err := validateOperands(a, b)
+	if !valid {
+		return 0, err
+	}
+
+	return a + b, nil
+}
+
+func subtract(a float32, b float32) (float32, error) {
+	valid, err := validateOperands(a, b)
+	if !valid {
+		return 0, err
+	}
+
+	return a - b, nil
+}
+
+func multiply(a float32, b float32) (float32, error) {
+	valid, err := validateOperands(a, b)
+	if !valid {
+		return 0, err
+	}
+
+	return a * b, nil
+}
+
+func divide(a float32, b float32) (float32, error) {
+	valid, err := validateOperands(a, b)
+	if !valid {
+		return 0, err
+	}
+
+	divisionResult := a / b
+
+	if math.IsInf(float64(divisionResult), 0) {
+		return 0, errors.New("Can't divide by zero!")
+	}
+
+	return a / b, nil
+}
+
+func validateOperands(x float32, y float32) (bool, error) {
+	// check whether a or b are infinite (+ve and -ve alike)
+	// good to see infinity checkers already included in the std lib
+	if math.IsInf(float64(x), 0) || math.IsInf(float64(y), 0) {
+		return false, errors.New("Operands can't be infinite!")
+	}
+
+	return true, nil
 }
